7452985831be6f7f283a7faf1f2ad70b9d1f2f28
BaoLH1908 <hoangbao.lhb@gmail.com>
2024-12-17 14:59:45 +0700

    Create 4.1.c

---
+++ Lab 2 - Mảng và chuỗi/4.1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    int arr[100];
+    int i, n;
+    // Câu 1
+    printf("Nhap do dai mang: n = ");
+    scanf("%d", &n);
+    for (i = 0; i < n; i++){
+        printf("Nhap arr[%d] = ", i);
+        scanf("%d", &arr[i]);
+    }
+    // Câu 2
+    printf("arr[] = ");
+    for (i = 0; i < n; i++){
+        printf("%d ", arr[i]);
+    }
+    return 0;
+}
